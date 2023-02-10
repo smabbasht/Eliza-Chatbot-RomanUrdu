72f3dc7d39febb6191dfdffc0bd80ad7c11b4eda
Syed M. Abbas Haider Taqvi <abbastaqvi81@gmail.com>
2023-02-11 00:03:06 +0500

    update prompts in roman urdu

--- eliza_chatbot.py
+++ eliza_chatbot.py
@@ -20,298 +20,212 @@ from nltk.chat.util import Chat, reflections
 
 pairs = (
     (
-        r"I need (.*)",
+        " (.*)",
         (
-            "Why do you need %1?",
-            "Would it really help you to get %1?",
-            "Are you sure you need %1?",
+            "Aap ko %1 kiu chahiye?",
+            "Kia ye waqie apki madad krega agr apko %1 ye miljae?",
+            "Kia apko yaqeen hai ke ap ko %1 chahiye?",
         ),
     ),
     (
-        r"Why don\'t you (.*)",
+        r"Tum kiu (.*) nahi kar rahi?",
         (
-            "Do you really think I don't %1?",
-            "Perhaps eventually I will %1.",
-            "Do you really want me to %1?",
+            "Kia aap waqie sochte hain ke main %1 nahi kar rahi hun?",
+            "Shyd bil aakhir main %1 krlun.",
+            "Kia ap waqie chahtay hain ke main %1 kru?",
         ),
     ),
     (
-        r"Why can\'t I (.*)",
+        r"Main kiu (.*) nahi kar sakta?",
         (
-            "Do you think you should be able to %1?",
-            "If you could %1, what would you do?",
-            "I don't know -- why can't you %1?",
-            "Have you really tried?",
+            "Kia aap ko lagta hai ke aap %1 kar sakenge?",
+            "Agar aap %1 kar saktay, to aap kia kartay?",
+            "Mujeh nahi pata, aap kiu %1 nahi krrhe?",
+            "Kia aapne waqie koshish ki hai?",
         ),
     ),
     (
-        r"I can\'t (.*)",
+        r"Main (.*) nahi kar sakta",
         (
-            "How do you know you can't %1?",
-            "Perhaps you could %1 if you tried.",
-            "What would it take for you to %1?",
+            "Aap ko kese pata ke aap %1 nahi kar saktay?",
+            "Zahiran aap %1 kar saktay agar aap koshish kartay.",
         ),
     ),
     (
-        r"I am (.*)",
+        r"Main (.*) hun",
         (
-            "Did you come to me because you are %1?",
-            "How long have you been %1?",
-            "How do you feel about being %1?",
+            "Kia aap mere pas ae hain kiunke aap %1 hain?",
+            "Aap kab se %1 hain?",
+            "Aap ko %1 hote hue kesa mehsoos horha hai?",
         ),
     ),
     (
-        r"I\'m (.*)",
+        r"Main (.*) karta hun",
         (
-            "How does being %1 make you feel?",
-            "Do you enjoy being %1?",
-            "Why do you tell me you're %1?",
-            "Why do you think you're %1?",
+            "Aap ko %1 hona kesa lagta hai?",
+            "Kia aap %1 honay enjoy krtay hain?",
         ),
     ),
     (
-        r"Are you (.*)",
+        r"Kia aap (.*) hain?",
         (
-            "Why does it matter whether I am %1?",
-            "Would you prefer it if I were not %1?",
-            "Perhaps you believe I am %1.",
-            "I may be %1 -- what do you think?",
+            "Isse kiu farq prta hai ke main %1 hun ya nahi?",
+            "Kia ye behtar hota agar main %1 hoti?",
         ),
     ),
     (
-        r"What (.*)",
+        r"Kia (.*)",
         (
-            "Why do you ask?",
-            "How would an answer to that help you?",
-            "What do you think?",
+            "Aap ye kiu puchre hain?",
+            "Iska jawab apki kis tarhan madad karega",
+            "Aap kia sochte hain is baray main?",
         ),
     ),
     (
-        r"How (.*)",
+        r"Kese (.*)",
         (
-            "How do you suppose?",
-            "Perhaps you can answer your own question.",
-            "What is it you're really asking?",
+            "Aap kia samjhtay hain is baray main?",
+            "Ghaliban ap apnay sawal ka khud hi jawab deskte hain."
         ),
     ),
     (
-        r"Because (.*)",
+        r"Kiunke (.*)",
         (
-            "Is that the real reason?",
-            "What other reasons come to mind?",
-            "Does that reason apply to anything else?",
-            "If %1, what else must be true?",
+            "Kia yehi asal wajah hai?",
+            "Kia koi aur wajah apke zehn main aarahi hai?",
+            "Kia ye wajah kisi or msle kesath bhi hai?",
+            "Agar %1 hai to mazeed kia kia hoga?",
         ),
     ),
     (
-        r"(.*) sorry (.*)",
+        r"(.*) mazrat (.*)",
         (
-            "There are many times when no apology is needed.",
-            "What feelings do you have when you apologize?",
+            "Basa Auqaat apki mazrat hi darkaar hoti hai",
+            "Apki jazbat kis tarha hote hain jab aap mazrat krte hain?",
         ),
     ),
     (
-        r"Hello(.*)",
+        r"Salam (.*)",
         (
-            "Hello... I'm glad you could drop by today.",
-            "Hi there... how are you today?",
-            "Hello, how are you feeling today?",
+            "Waalaikum Asalam, Main bohat khush hun ke aap aaj agaye",
         ),
     ),
     (
-        r"I think (.*)",
-        ("Do you doubt %1?", "Do you really think so?", "But you're not sure %1?"),
+        r"Mujhe lagta hai (.*)",
+        ("Kia apko %1 main koi shak hai?", "Kia aapko waqie ye lagta hai?"),
     ),
     (
-        r"(.*) friend (.*)",
+        r"(.*) dost (.*)",
         (
-            "Tell me more about your friends.",
-            "When you think of a friend, what comes to mind?",
-            "Why don't you tell me about a childhood friend?",
-        ),
-    ),
-    (r"Yes", ("You seem quite sure.", "OK, but can you elaborate a bit?")),
-    (
-        r"(.*) computer(.*)",
-        (
-            "Are you really talking about me?",
-            "Does it seem strange to talk to a computer?",
-            "How do computers make you feel?",
-            "Do you feel threatened by computers?",
+            "Apne dosto ke baaray main mazeed bataiye",
+            "Jab aap dosto ke baray main sochte hain to apke zehn mn kia aata hai?",
         ),
     ),
     (
-        r"Is it (.*)",
+        r"Kia aap (.*)?",
         (
-            "Do you think it is %1?",
-            "Perhaps it's %1 -- what do you think?",
-            "If it were %1, what would you do?",
-            "It could well be that %1.",
+            "Aap kiu sochte hain ke main %1 nahi kar sakti?",
+            "Aap kiu puchrhe hain ke main %1 kar sakti hu ya nahi?",
         ),
     ),
     (
-        r"It is (.*)",
+        r"Kia Main (.*)\?",
         (
-            "You seem very certain.",
-            "If I told you that it probably isn't %1, what would you feel?",
+            "To zahiran aap %1 nahi chatay?",
+            "Kia aap waqian %1 krna chahtay hain?",
         ),
     ),
     (
-        r"Can you (.*)",
+        r"Kia Kaheen (.*)",
         (
-            "What makes you think I can't %1?",
-            "If I could %1, then what?",
-            "Why do you ask if I can %1?",
+            "Kia aapko lgta hai ke %1 hai?",
+            "Ji, ye mumkin hai ke %1 ho",
         ),
     ),
     (
-        r"Can I (.*)",
+        r"Main (.*)",
         (
-            "Perhaps you don't want to %1.",
-            "Do you want to be able to %1?",
-            "If you could %1, would you?",
+            "Main samjh rahi hu",
+            "Aap kiu kehrhe hain ke aap %1 hain?",
         ),
     ),
     (
-        r"You are (.*)",
+        r"Aap (.*)",
         (
-            "Why do you think I am %1?",
-            "Does it please you to think that I'm %1?",
-            "Perhaps you would like me to be %1.",
-            "Perhaps you're really talking about yourself?",
+            "Hamein apke baray main baat karni chahiye, mere baray main nahi",
+            "Aap mere baray main ye kiu kehrhe hain?",
+            "Aap ko kia farq par skta hain mere %1 hone se?",
         ),
     ),
     (
-        r"You\'re (.*)",
+        r"Kiu (.*)",
         (
-            "Why do you say I am %1?",
-            "Why do you think I am %1?",
-            "Are we talking about you, or me?",
+            "Aap mujhe is baray main mazeed bataenge?",
+            "Aap kiu sochte hain %1"
+        )
+    ),
+    (
+        r"Main chahta hu (.*)",
+        (
+            "Kia hoga agar apko %1 mil jaye?",
+            "Aap %1 kiu chahtay hain?",
         ),
     ),
     (
-        r"I don\'t (.*)",
-        ("Don't you really %1?", "Why don't you %1?", "Do you want to %1?"),
-    ),
-    (
-        r"I feel (.*)",
+        r"(.*) walida(.*)",
         (
-            "Good, tell me more about these feelings.",
-            "Do you often feel %1?",
-            "When do you usually feel %1?",
-            "When you feel %1, what do you do?",
+            "Apni walida ke baray main mazeed bataiye",
+            "Aapki walida apko kesa mehsoos krwatay thay?",
+            "Apko apnai walida ke baray main kia lgta hai?",
+            "Kia aapka apni waida se taaluq apki aaj ke ahsaasat ki wajah hai?",
         ),
     ),
     (
-        r"I have (.*)",
+        r"(.*) walid(.*)",
         (
-            "Why do you tell me that you've %1?",
-            "Have you really %1?",
-            "Now that you have %1, what will you do next?",
+            "Apne walid ke baray main mazeed bataiye",
+            "Aapke walid apko kesa mehsoos krwatay thay?",
+            "Apko apnay walid ke baray main kia lgta hai?",
+            "Kia aapka apne waid se taaluq apki aaj ke ahsaasat ki wajah hai?",
         ),
     ),
     (
-        r"I would (.*)",
+        r"(.*) bachpan(.*)",
         (
-            "Could you explain why you would %1?",
-            "Why would you %1?",
-            "Who else knows that you would %1?",
-        ),
-    ),
-    (
-        r"Is there (.*)",
-        (
-            "Do you think there is %1?",
-            "It's likely that there is %1.",
-            "Would you like there to be %1?",
-        ),
-    ),
-    (
-        r"My (.*)",
-        (
-            "I see, your %1.",
-            "Why do you say that your %1?",
-            "When your %1, how do you feel?",
-        ),
-    ),
-    (
-        r"You (.*)",
-        (
-            "We should be discussing you, not me.",
-            "Why do you say that about me?",
-            "Why do you care whether I %1?",
-        ),
-    ),
-    (r"Why (.*)", ("Why don't you tell me the reason why %1?", "Why do you think %1?")),
-    (
-        r"I want (.*)",
-        (
-            "What would it mean to you if you got %1?",
-            "Why do you want %1?",
-            "What would you do if you got %1?",
-            "If you got %1, then what would you do?",
-        ),
-    ),
-    (
-        r"(.*) mother(.*)",
-        (
-            "Tell me more about your mother.",
-            "What was your relationship with your mother like?",
-            "How do you feel about your mother?",
-            "How does this relate to your feelings today?",
-            "Good family relations are important.",
-        ),
-    ),
-    (
-        r"(.*) father(.*)",
-        (
-            "Tell me more about your father.",
-            "How did your father make you feel?",
-            "How do you feel about your father?",
-            "Does your relationship with your father relate to your feelings today?",
-            "Do you have trouble showing affection with your family?",
-        ),
-    ),
-    (
-        r"(.*) child(.*)",
-        (
-            "Did you have close friends as a child?",
-            "What is your favorite childhood memory?",
-            "Do you remember any dreams or nightmares from childhood?",
-            "Did the other children sometimes tease you?",
-            "How do you think your childhood experiences relate to your feelings today?",
+            "Kia aap ke bachpan mn qareebi dost the?",
+            "Aapke bachpan ki koi dilchaspt yaadein?",
+            "Kia aap ko koi bachpan ka acha ya khofnak khuwab yaad hai?",
+            "Kia dusre bachay apko kabhi kabhar tang kia kartay thay?",
         ),
     ),
     (
         r"(.*)\?",
         (
-            "Why do you ask that?",
-            "Please consider whether you can answer your own question.",
-            "Perhaps the answer lies within yourself?",
-            "Why don't you tell me?",
+            "Aap ye kiu puchrhe hain?",
+            "Aap sochie kia ap apne sawal ka jawab deskte hain?",
+            "Ghaliban jawab ap ke andar hi chupa hai",
+            "Aap mujhe kiu nahi bata dete?",
         ),
     ),
     (
         r"quit",
         (
-            "Thank you for talking with me.",
-            "Good-bye.",
-            "Thank you, that will be $150.  Have a good day!",
+            "Mujhse guftugu farmanay ka shukria",
+            "Khuda Hafez",
+            "Shukria, ye 2000 rs. honge",
         ),
     ),
     (
         r"(.*)",
         (
-            "Please tell me more.",
-            "Let's change focus a bit... Tell me about your family.",
-            "Can you elaborate on that?",
-            "Why do you say that %1?",
-            "I see.",
-            "Very interesting.",
+            "Baraye mehrbani, mazeed btayie",
+            "Chlen kuch aur baat karte hain, apni family ke baray main kuch batayie",
+            "Kia iski wazahat kar sakte hain",
+            "Aap %1 kese kehrhe hain?",
+            "Sahi Sahi",
+            "Ye dilchasp baat hai",
             "%1.",
-            "I see.  And what does that tell you?",
-            "How does that make you feel?",
-            "How do you feel when you say that?",
+            "Acha. Aur isse aap kia andaza lagatay hain?",
         ),
     ),
 )
@@ -321,18 +235,16 @@ eliza_chatbot = Chat(pairs, reflections)
 
 def eliza_chat():
     print("Therapist\n---------")
-    print("Talk to the program by typing in plain English, using normal upper-")
-    print('and lower-case letters and punctuation.  Enter "quit" when done.')
+    print("Program se plain Roman main baat kijiye, aam upper-case, lower-case-")
+    print('letters or punctuations kesath. \nJab aap quit krna chahen to "khatam" type kijiye')
     print("=" * 72)
-    print("Hello.  How are you feeling today?")
+    print("Salam. Aap aaj kesa mehsoos krrhe hain?")
 
     eliza_chatbot.converse()
 
 
-
 def demo():
     eliza_chat()
-
 
 
 if __name__ == "__main__":
